d1373c2aaa1c797d82f87bd978a6c5d58b7f40a9
VaishnaviGanjare <vaishnaviganjare2018@gmail.com>
2024-06-08 09:30:21 +0530

    task

---
+++ backend/expressJs/app.js
@@ -0,0 +1,24 @@
+const http=require('http');
+const express=require('express');
+const app=express();
+
+console.log('starting application');
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log('In another middleware');
+    res.send('<h>Hello from NodeJs!</h>');
+    // const value='hi';
+    // res.send({key1:value});
+});
+
+// const server= http.createServer(app);
+// server.listen(4000);
+
+//above teo lines can be replace with below line as it does same work internally.
+app.listen(4000);
