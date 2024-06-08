2e2644fcab2eac268cdd36bdb5729b22c889a6e3
VaishnaviGanjare <vaishnaviganjare2018@gmail.com>
2024-06-08 23:10:28 +0530

    task 2

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-//const http=require('http');
+
 const express=require('express');
 const app=express();
+const bodyParser = require('body-parser');
 
-console.log('starting application');
+//console.log('starting application');
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST">product: <input type="text" name="title"><br>size:  <input type="number" name="size"><br><button type="submit">add product</button></form>');
 });
 
-app.use((req,res,next)=>{
-    console.log('In another middleware');
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
     res.send('<h>Hello from NodeJs!</h>');
-    // const value='hi';
-    // res.send({key1:value});
 });
 
-// const server= http.createServer(app);
-// server.listen(4000);
-
-//above teo lines can be replace with below line as it does same work internally.
 app.listen(4000);
