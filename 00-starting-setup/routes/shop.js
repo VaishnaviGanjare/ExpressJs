fdb1d36b1531a05038d7f6a4d1a0d31064aa2bd0
VaishnaviGanjare <vaishnaviganjare2018@gmail.com>
2024-06-18 21:55:30 +0530

    task 8 with ejs and added product detail functionality

---
+++ 00-starting-setup/routes/shop.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);//:=>defines wordss after : can be anything and not productId as we are setting id.
+//but if we want different segment like /delete then we need to declare it first before this route otherwise it will not include as it will search for new productId  
+//only
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
