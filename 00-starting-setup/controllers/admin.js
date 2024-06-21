9e8b60051953a6436a07d23b8254e2d500a98239
VaishnaviGanjare <vaishnaviganjare2018@gmail.com>
2024-06-21 11:59:13 +0530

    task10 added edit and delete functionality

--- 00-starting-setup/controllers/admin.js
+++ 00-starting-setup/controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false
   });
 };
 
@@ -15,10 +13,46 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, price, description);
   product.save();
   res.redirect('/');
 };
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode=req.query.edit;
+  if(!editMode){
+    return res.redirect('/');
+  }
+  console.log(req.params);
+  const prodId=req.params.productId;
+  console.log(prodId);
+  Product.findById(prodId, (product)=>{
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product || {}
+    });
+  });
+};
+
+exports.postEditProduct = (req,res,next)=>{
+  const prodId=req.body.productId;
+  const updatedtitle=req.body.title;
+  const updatedimageUrl=req.body.imageUrl;
+  const updatedprice=req.body.price;
+  const updateddescription=req.body.description;
+
+  const updatedProduct=new Product(
+    prodId, 
+    updatedtitle, 
+    updatedimageUrl, 
+    updatedprice, 
+    updateddescription
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
+}
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -29,3 +63,10 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+exports.postDeleteProduct = (req, res, next)=>{
+  const prodId=req.body.productId;
+  console.log(prodId);
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
+}
--- 00-starting-setup/models/product.js
+++ 00-starting-setup/models/product.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const Cart=require('./cart');
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
@@ -18,20 +19,34 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, price, description) {
+    this.id=id;
     this.title = title;
     this.imageUrl = imageUrl;
-    this.description = description;
     this.price = price;
+    this.description = description;
   }
 
-  save() {
-    this.id = Math.random().toString();
+  save() {    
     getProductsFromFile(products => {
+      if(this.id){
+        const existingProductIndex=products.findIndex(
+          prod=> prod.id===this.id
+        );
+        const updatedProducts=[...products];
+        updatedProducts[existingProductIndex]=this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      }
+      else
+     { 
+      this.id = Math.random().toString();
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
+    }
     });
   }
 
@@ -43,6 +58,16 @@ module.exports = class Product {
     getProductsFromFile(products=>{
       const product=products.find(p=> p.id==id);
       cb(product);
+    });
+  }
+
+  static deleteById(id){
+    getProductsFromFile(products=>{
+      const product=products.find(prod=>prod.id===id)
+      const updatedProducts=products.filter(p=> p.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err=>{
+        Cart.deleteProduct(id, product.price);
+      })
     });
   }
 };
