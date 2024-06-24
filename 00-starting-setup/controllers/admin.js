6d92075812a17abae970adc0590f380ad7f8f5c6
VaishnaviGanjare <vaishnaviganjare2018@gmail.com>
2024-06-24 12:05:04 +0530

    task16 delete functionality with sql added edit is not working

--- 00-starting-setup/controllers/admin.js
+++ 00-starting-setup/controllers/admin.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product');
+const Product = require('../models/product'); 
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -13,9 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, price, description);
-  product.save();
-  res.redirect('/');
+  const product = new Product(title, imageUrl, price, description);
+  product.save()
+  .then(()=>{
+    res.redirect('/');
+  })
+  .catch((err)=>{
+    console.log(err);
+  });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -26,14 +31,18 @@ exports.getEditProduct = (req, res, next) => {
   console.log(req.params);
   const prodId=req.params.productId;
   console.log(prodId);
-  Product.findById(prodId, (product)=>{
+  Product.findById()
+  .then(([product])=>{
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
-      product: product || {}
+      product: product[0]
     });
-  });
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
 };
 
 exports.postEditProduct = (req,res,next)=>{
@@ -50,23 +59,38 @@ exports.postEditProduct = (req,res,next)=>{
     updatedprice, 
     updateddescription
   );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  updatedProduct.edit(prodId)
+  .then(([product])=>{
+    res.redirect('/admin/products');
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+  
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([product])=>{
     res.render('admin/products', {
-      prods: products,
+      prods: product,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
-  });
+  })
+  .catch((err)=>{
+   console.log(err);
+  })
 };
 
 exports.postDeleteProduct = (req, res, next)=>{
   const prodId=req.body.productId;
   console.log(prodId);
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.deleteById(prodId)
+  .then(([product])=>{
+    res.redirect('/admin/products');
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
 }
--- 00-starting-setup/models/product.js
+++ 00-starting-setup/models/product.js
@@ -1,22 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const db=require('../util/database');
 const Cart=require('./cart');
-
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
 
 module.exports = class Product {
   constructor(id, title, imageUrl, price, description) {
@@ -27,47 +10,28 @@ module.exports = class Product {
     this.description = description;
   }
 
-  save() {    
-    getProductsFromFile(products => {
-      if(this.id){
-        const existingProductIndex=products.findIndex(
-          prod=> prod.id===this.id
-        );
-        const updatedProducts=[...products];
-        updatedProducts[existingProductIndex]=this;
-        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-          console.log(err);
-        });
-      }
-      else
-     { 
-      this.id = Math.random().toString();
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
-    }
-    });
+  save() {
+     
+    return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES(?,?,?,?)',
+      [this.title, this.price, this.imageUrl, this.description]);
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
+  static edit(id){
+    return db.execute(
+      'UPDATE products SET title = ?, price = ?, imageUrl = ?, description = ? WHERE id = ?',
+      [this.title, this.price, this.imageUrl, this.description, this.id]
+    );
   }
 
-  static findById(id, cb){
-    getProductsFromFile(products=>{
-      const product=products.find(p=> p.id==id);
-      cb(product);
-    });
+  static fetchAll() {
+    return db.execute('SELECT * FROM products');
+  }
+
+  static findById(id){
+    return db.execute('SELECT * FROM products WHERE products.id = ?',[id]);
   }
 
   static deleteById(id){
-    getProductsFromFile(products=>{
-      const product=products.find(prod=>prod.id===id)
-      const updatedProducts=products.filter(p=> p.id !== id);
-      fs.writeFile(p, JSON.stringify(updatedProducts), err=>{
-        Cart.deleteProduct(id, product.price);
-      })
-    });
+    return db.execute('DELETE FROM products WHERE products.id=?',[id]);
   }
 };
